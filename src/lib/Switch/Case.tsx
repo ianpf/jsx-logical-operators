108d032a3d1bd75289b79cd686a0fd7c4cd155b0
Ian Fisher <ian@zaius.com>
2020-05-21 09:30:38 -0400

    fixing issue where switch returned all children if when was wrong

--- src/lib/Switch/Case.tsx
+++ src/lib/Switch/Case.tsx
@@ -1,7 +1,7 @@
 import React, { FunctionComponent } from 'react'
 
 export interface CaseProps {
-  when: string | number | object | boolean | ((value: string | object | number) => boolean)
+  when: string | number | object | boolean | ((value?: string | object | number | null) => boolean)
 }
 
 export const Case: FunctionComponent<CaseProps> = ({ children }) => {
--- src/lib/Switch/Switch.tsx
+++ src/lib/Switch/Switch.tsx
@@ -9,20 +9,16 @@ export interface SwitchProps {
    * If provided, this will be tested for equality using triple equals, so in the case of objects it will be _referential equality_.
    * If this is not provided, then the case statements will be tested for "truthiness" if values are provided, and will be executed if functions are provided,
    */
-  on?: string | number | object
+  on?: string | number | object | null
   children: Many<JSX.Element>
 }
 
 export const Switch: FunctionComponent<SwitchProps> = ({ children, on }) => {
-  if (on != null) {
-    const [, cases, defaults] = groupChildren(wrapChildren(children), Case, Default)
-    return (
-      cases.find(({ props: { when } }) => {
-        return (typeof when === 'function' && when(on)) || (on == null && when) || when === on
-      }) ||
-      defaults[0] || <></>
-    )
-  } else {
-    return <>{children}</>
-  }
+  const [, cases, defaults] = groupChildren(wrapChildren(children), Case, Default)
+  return (
+    cases.find(({ props: { when } }) => {
+      return (typeof when === 'function' && when(on)) || (on == null && when) || when === on
+    }) ||
+    defaults[0] || <></>
+  )
 }
